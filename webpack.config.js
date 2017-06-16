d6cd023f0631c94256049a37e69c64831b860a11
pjsinco <psinco@gmail.com>
2017-06-16 12:59:48 -0500

    First commmit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+
+  entry: {
+    app: [ path.resolve(__dirname, 'src', 'scripts', 'main.js') ],
+  },
+
+
+  output: {
+    path: path.join(__dirname, 'public'),
+    publicPath: '/public/',
+    filename: 'bundle.js',
+  },
+
+  module: {
+
+    loaders: [
+      {
+        test: /\.jade$/,
+        include: path.resolve(__dirname, 'src', 'views'),
+        loader: 'raw!jade-html',
+      },
+    ],
+  },
+};
+
